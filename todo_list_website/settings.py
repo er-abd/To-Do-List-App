33d820e623e45b66b9fd1dd4aa7f90d90ec9cd2b
er-abd <108542244+er-abd@users.noreply.github.com>
2022-09-04 20:47:45 +0100

    first commit

--- todo_list_website/settings.py
+++ todo_list_website/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 
 import os
+import django_heroku
 
 from pathlib import Path
 
@@ -139,6 +140,7 @@ LOGIN_URL = 'login'
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+django_heroku.settings(locals())
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
