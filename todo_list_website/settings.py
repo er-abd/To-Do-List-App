ce0b67b76b185d4fae736da477f517ac6ad819dc
er-abd <108542244+er-abd@users.noreply.github.com>
2022-09-05 18:49:55 +0100

    first commit

--- todo_list_website/settings.py
+++ todo_list_website/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = 'django-insecure-13aslrm9+ku(7^a)9^e&_$*294&w$3+h7qyzkmabf5k=_v9hy8
 DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1',
-                 'tick-task.herokuapp.com', 'www.tick-task.alabdullah.me', 'tick-task.alabdullah.me']
+                 'tick-task.herokuapp.com', 'www.tick-task.alabdullah.me', 'tick-task.alabdullah.me', 'alabdullah.me', 'www.alabdullah.me']
 
 
 # Application definition
